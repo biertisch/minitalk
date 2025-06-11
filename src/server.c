ac32bf380e62e8c86c1bd49abbaffa2b23b1dc7d
Beatriz De Almeida Rodrigues <beatde-a@c2r3s8.42lisboa.com>
2025-06-11 17:08:45 +0100

    modified

--- src/server.c
+++ src/server.c
@@ -12,6 +12,44 @@
 
 #include "../include/minitalk.h"
 
+char	*g_msg;
+
+static void	mem_check(char *s)
+{
+	if (!s)
+	{
+		ft_printf("Error: memory allocation failed.\n");
+		exit(1);
+	}
+}
+
+static void	assemble_msg(char c)
+{
+	char	*tmp;
+	char	append[2];
+
+	append[0] = c;
+	append[1] = '\0';
+	if (!g_msg)
+	{
+		g_msg = ft_strdup(append);
+		mem_check(g_msg);
+	}
+	else
+	{
+		tmp = g_msg;
+		g_msg = ft_strjoin(tmp, append);
+		free(tmp);
+		mem_check(g_msg);
+	}
+	if (c == '\0')
+	{
+		ft_printf("%s\n", g_msg);
+		free(g_msg);
+		g_msg = NULL;
+	}
+}
+
 static void	handler(int sig, siginfo_t *info, void *context)
 {
 	static unsigned char	c;
@@ -23,9 +61,7 @@ static void	handler(int sig, siginfo_t *info, void *context)
 	bit++;
 	if (bit == 8)
 	{
-		write(1, &c, 1);
-		if (c == '\0')
-			write(1, "\n", 1);
+		assemble_msg(c);
 		bit = 0;
 		c = 0;
 	}
